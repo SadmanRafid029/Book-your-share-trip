1b43c59b1ff82273a137965bc8b3f63f83a15608
Sadman Rafid <163814779+SadmanRafid029@users.noreply.github.com>
2024-04-21 22:10:53 +0600

    admin menu added

--- admin_menu.php
+++ admin_menu.php
@@ -65,19 +65,6 @@ while($row= mysqli_fetch_array($result)){
 <a href="admin_add.php"><button>Add Admin</button></a><br><br>
 
 </center>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6284cafcb0d10b6f3e72ce2a/1g3bbhrd9';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
 </body>
 </head>
 </html>
